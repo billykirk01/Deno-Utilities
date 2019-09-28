abe292af8cd996298743db44c647ab8ee8520c86
William Kirk <30280510+wkirk01@users.noreply.github.com>
2019-09-27 23:02:43 -0500

    Adding exists and removeFIle

--- fs.ts
+++ fs.ts
@@ -4,3 +4,25 @@ import { resolve } from "https://deno.land/std/fs/path/mod.ts";
 export function readJsonSync(fileName: string): unknown {
     return _readJsonSync(resolve(fileName))
 }
+
+export function removeFile(fileName: string){
+    if (exists(fileName)){
+        Deno.removeSync(resolve(fileName))
+    }
+}
+
+export function exists(filename: string): boolean {
+    try {
+      Deno.statSync(resolve(filename));
+      // successful, file or directory must exist
+      return true;
+    } catch (error) {
+      if (error && error.kind === Deno.ErrorKind.NotFound) {
+        // file or directory does not exist
+        return false;
+      } else {
+        // unexpected error, maybe permissions, pass it along
+        throw error;
+      }
+    }
+  };

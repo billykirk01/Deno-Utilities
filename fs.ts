6d701aab9f46a7e6d1c7c623dafcef4ba9d12d4f
Billy Kirk <30280510+wkirk01@users.noreply.github.com>
2020-05-18 17:51:42 -0500

    Removing unnecessary functions

--- fs.ts
+++ fs.ts
@@ -1,65 +1,32 @@
-import { readJsonSync, writeJsonSync } from "https://deno.land/std/fs/mod.ts";
-import { resolve } from "https://deno.land/std/fs/path/mod.ts";
+import { readJson, writeJson } from "https://deno.land/std/fs/mod.ts";
 
-export function fileExists(filename: string): boolean {
-    try {
-        Deno.statSync(resolve(filename));
-        // successful, file or directory must exist
-        return true;
-    } catch (error) {
-        if (error && error.kind === Deno.ErrorKind.NotFound) {
-            // file or directory does not exist
-            return false;
-        } else {
-            // unexpected error, maybe permissions, pass it along
-            throw error;
-        }
-    }
-}
-
-export function removeFile(fileName: string) {
-    if (fileExists(fileName)) {
-        Deno.removeSync(resolve(fileName))
-    }
-}
-
-export function writeFileStringSync(
+/**
+ * Write the string to file.
+ *
+ * @param filename File to write
+ * @param content The content write to file
+ * @returns Promise<void>
+ */
+export async function appendFileStr(
     filename: string,
-    content: string,
-    options: Deno.WriteFileOptions
-) {
-    const filePath = resolve(filename)
+    content: string
+): Promise<void> {
     const encoder = new TextEncoder();
-    Deno.writeFileSync(filePath, encoder.encode(content), options);
+    await Deno.writeFile(filename, encoder.encode(content), { append: true });
 }
 
-export function appendLineToFile(filename: string, content: string) {
-    writeFileStringSync(filename, content + "\n", { append: true })
-}
-
-export function readJSONSync<T>(fileName: string): T {
-    return readJsonSync(resolve(fileName)) as T
-}
-
-export function writeJSONSync(fileName: string, json: any) {
-    return writeJsonSync(resolve(fileName), json)
-}
-
-export function transformJSONArray<T>(
-    inputFileName: string,
-    outputFileName: string,
-    transformFn?: (obj: T) => any
+export async function transformJSONArray(
+    inputFile: string,
+    outputFile: string,
+    transformFn: (obj: any) => any
 ) {
-    const inputFile = resolve(inputFileName)
-    const outputFile = resolve(outputFileName)
-    const inputJSON = readJSONSync(inputFile) as T[]
-    if (transformFn) {
-        const outputJSON = inputJSON.map(transformFn)
-        writeJSONSync(outputFile, outputJSON)
-        return
+    const inputJSON = await readJson(inputFile) as any[];
+    if (transformFn && typeof (transformFn) === 'function') {
+        const outputJSON = inputJSON.map(transformFn);
+        writeJson(outputFile, outputJSON);
+        return;
     }
-    writeJSONSync(outputFile, inputJSON)
+    writeJson(outputFile, inputJSON);
 }
 
-export * from "https://deno.land/std/fs/mod.ts"
-export { resolve } from "https://deno.land/std/fs/path/mod.ts";
+export * from "https://deno.land/std/fs/mod.ts";

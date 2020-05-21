0c826c2a628e092ab45bc001f69188ccef92165c
William Kirk <30280510+wkirk01@users.noreply.github.com>
2020-05-21 00:04:21 -0500

    rename temp array

--- stream-json.ts
+++ stream-json.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from "https://deno.land/std/node/events.ts";
 class JSONStream extends EventEmitter {
 
     private openBraceCount = 0;
-    private tempByteArray: number[] = [];
+    private tempUint8Array: number[] = [];
     private decoder = new TextDecoder();
 
     constructor (private filepath: string) {
@@ -14,22 +14,22 @@ class JSONStream extends EventEmitter {
     async stream() {
         console.time("Run Time");
         let file = await Deno.open(this.filepath);
-        //creates async iterator from reader, default buffer size is 32kb
+        //creates iterator from reader, default buffer size is 32kb
         for await (const buffer of Deno.iter(file)) {
             buffer.forEach((uint8) => {
                 //open brace
                 if (uint8 === 123) {
-                    if (this.openBraceCount === 0) this.tempByteArray = [];
+                    if (this.openBraceCount === 0) this.tempUint8Array = [];
                     this.openBraceCount++;
                 };
 
-                this.tempByteArray.push(uint8);
+                this.tempUint8Array.push(uint8);
 
                 //close brace
                 if (uint8 === 125) {
                     this.openBraceCount--;
                     if (this.openBraceCount === 0) {
-                        const uint8Ary = new Uint8Array(this.tempByteArray);
+                        const uint8Ary = new Uint8Array(this.tempUint8Array);
                         const jsonString = this.decoder.decode(uint8Ary);
                         const object = JSON.parse(jsonString);
                         this.emit('object', object);

f50319443fb731dd904c9c1c1e7eeefe41d2576f
Billy Kirk <kirk.william.c@gmail.com>
2020-11-11 08:45:37 -0600

    Updating to latest standard library

--- fs.ts
+++ fs.ts
@@ -1,4 +1,3 @@
-import { readJson, writeJson } from "https://deno.land/std@0.77.0/fs/mod.ts";
 import { BufReader, readLines } from "https://deno.land/std@0.77.0/io/mod.ts";
 
 /**
@@ -9,28 +8,26 @@ import { BufReader, readLines } from "https://deno.land/std@0.77.0/io/mod.ts";
  * @returns Promise<void>
  */
 export async function appendFileStr(
-    filename: string,
-    content: string
+  filename: string,
+  content: string,
 ): Promise<void> {
-    const encoder = new TextEncoder();
-    await Deno.writeFile(filename, encoder.encode(content), { append: true });
+  const encoder = new TextEncoder();
+  await Deno.writeFile(filename, encoder.encode(content), { append: true });
 }
 
 export async function transformJSONArray(
-    inputFile: string,
-    outputFile: string,
-    transformFn: (obj: any) => any
+  inputFile: string,
+  outputFile: string,
+  transformFn: (obj: any) => any,
 ) {
-    const inputJSON = await readJson(inputFile) as any[];
-    if (transformFn && typeof (transformFn) === 'function') {
-        const outputJSON = inputJSON.map(transformFn);
-        writeJson(outputFile, outputJSON);
-        return;
-    }
-    writeJson(outputFile, inputJSON);
+  const inputJSON = await readJson(inputFile) as any[];
+  if (transformFn && typeof (transformFn) === "function") {
+    const outputJSON = inputJSON.map(transformFn);
+    writeJson(outputFile, outputJSON);
+    return;
+  }
+  writeJson(outputFile, inputJSON);
 }
-
-
 
 /** Iterates through a file line by line.
  * @param filename File to read
@@ -45,6 +42,35 @@ export async function* readline(filename: string): AsyncGenerator<string> {
   }
 
   r.close();
+}
+
+/** Reads a JSON file and then parses it into an object */
+export async function readJson(filePath: string): Promise<unknown> {
+  const decoder = new TextDecoder("utf-8");
+
+  const content = decoder.decode(await Deno.readFile(filePath));
+
+  try {
+    return JSON.parse(content);
+  } catch (err) {
+    err.message = `${filePath}: ${err.message}`;
+    throw err;
+  }
+}
+
+/* Writes an object to a JSON file. */
+export async function writeJson(
+  filePath: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  object: any,
+  options: WriteJsonOptions = {},
+): Promise<void> {
+  const jsonString = serialize(filePath, object, options);
+  await Deno.writeTextFile(filePath, jsonString, {
+    append: options.append,
+    create: options.create,
+    mode: options.mode,
+  });
 }
 
 export * from "https://deno.land/std/fs/mod.ts";

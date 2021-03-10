4ea910057f21af4d40e822e953d6b2cdeca1e910
wkirk01 <kirk.william.c@gmail.com>
2021-03-10 10:44:36 -0600

    Commenting out the file

--- stream-json.ts
+++ stream-json.ts
@@ -1,55 +1,55 @@
-import { EventEmitter } from "https://deno.land/std/node/events.ts";
+// import { EventEmitter } from "https://deno.land/std/node/events.ts";
 
-export class JSONStream extends EventEmitter {
+// export class JSONStream extends EventEmitter {
 
-    private openBraceCount = 0;
-    private tempUint8Array: number[] = [];
-    private decoder = new TextDecoder();
+//     private openBraceCount = 0;
+//     private tempUint8Array: number[] = [];
+//     private decoder = new TextDecoder();
 
-    constructor (private filepath: string) {
-        super();
-        this.stream();
-    }
+//     constructor (private filepath: string) {
+//         super();
+//         this.stream();
+//     }
 
-    async stream() {
-        console.time("Run Time");
-        let file = await Deno.open(this.filepath);
-        //creates iterator from reader, default buffer size is 32kb
-        for await (const buffer of Deno.iter(file)) {
+//     async stream() {
+//         console.time("Run Time");
+//         let file = await Deno.open(this.filepath);
+//         //creates iterator from reader, default buffer size is 32kb
+//         for await (const buffer of Deno.iter(file)) {
 
-            for (let i = 0, len = buffer.length; i < len; i++) {
-                const uint8 = buffer[ i ];
+//             for (let i = 0, len = buffer.length; i < len; i++) {
+//                 const uint8 = buffer[ i ];
 
-                //remove whitespace
-                if (uint8 === 10 || uint8 === 13 || uint8 === 32) continue;
+//                 //remove whitespace
+//                 if (uint8 === 10 || uint8 === 13 || uint8 === 32) continue;
 
-                //open brace
-                if (uint8 === 123) {
-                    if (this.openBraceCount === 0) this.tempUint8Array = [];
-                    this.openBraceCount++;
-                };
+//                 //open brace
+//                 if (uint8 === 123) {
+//                     if (this.openBraceCount === 0) this.tempUint8Array = [];
+//                     this.openBraceCount++;
+//                 };
 
-                this.tempUint8Array.push(uint8);
+//                 this.tempUint8Array.push(uint8);
 
-                //close brace
-                if (uint8 === 125) {
-                    this.openBraceCount--;
-                    if (this.openBraceCount === 0) {
-                        const uint8Ary = new Uint8Array(this.tempUint8Array);
-                        const jsonString = this.decoder.decode(uint8Ary);
-                        const object = JSON.parse(jsonString);
-                        this.emit('object', object);
-                    }
-                };
-            };
-        }
-        file.close();
-        console.timeEnd("Run Time");
-    }
-}
+//                 //close brace
+//                 if (uint8 === 125) {
+//                     this.openBraceCount--;
+//                     if (this.openBraceCount === 0) {
+//                         const uint8Ary = new Uint8Array(this.tempUint8Array);
+//                         const jsonString = this.decoder.decode(uint8Ary);
+//                         const object = JSON.parse(jsonString);
+//                         this.emit('object', object);
+//                     }
+//                 };
+//             };
+//         }
+//         file.close();
+//         console.timeEnd("Run Time");
+//     }
+// }
 
-// const stream = new JSONStream('test.json');
+// // const stream = new JSONStream('test.json');
 
-// stream.on('object', (object: any) => {
-//      do something with object
-// });
+// // stream.on('object', (object: any) => {
+// //      do something with object
+// // });
